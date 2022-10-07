213a80ca12b40eacd8830796eed3d852aee1f6d2
Redoqx <ridho.pmswa@gmail.com>
2022-10-07 07:27:34 +0700

    Isi detail

--- task_2/kaMa.php
+++ task_2/kaMa.php
@@ -1,8 +1,10 @@
 <?php
     $id = $_GET['idProduct'];
+    $nego = $_GET['nego'];
+    if(!isset($nego)) $nego="Harga Awal";
     switch($id){
         case 1:
-            header("Location: LenovoIdeapad.html");
+            header("Location: LenovoIdeapad.html?penawaran=$nego");
             break;
     }
 ?>
